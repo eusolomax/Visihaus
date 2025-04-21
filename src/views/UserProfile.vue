3fe03507489d927beaf1f86e728fcac0519394a4
Victor Capitelli <victor.capitelli@scicrop.com>
2025-04-20 21:15:41 -0300

    feature: Vuetify implement; feature: User Profile WIP;

---
+++ src/views/UserProfile.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="d-flex flex-column align-center w-100 bg-yellow">
+    <!-- Banner -->
+    <div :style="{ backgroundImage: '' }" style="height: 265px; background-size: cover;" class="w-100 bg-red" />
+    <!-- Profile Picture -->
+    <div style="height: 200px; width: 200px; top: 165px;" class="bg-blue position-absolute" />
+  </div>
+
+  <div style="height: 200px;" class="w-100 bg-purple">a</div>
+  <v-division />
+</template>
+
+<script setup lang="ts"></script>
