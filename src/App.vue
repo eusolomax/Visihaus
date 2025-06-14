bd85cab827193a2f39086816a65c53f40208d603
Victor Capitelli <victor.capitelli@scicrop.com>
2025-06-14 14:04:57 -0300

    feature: app margin;

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <router-view />
+  <div class="vh-margin"><router-view /></div>
 </template>
 
 <script lang="ts" setup></script>
+
+<style scoped>
+.vh-margin {
+  margin: 0 100px
+}
+</style>
