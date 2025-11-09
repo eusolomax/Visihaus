d8305c9d39b182f0983623067637468c52194ede
Victor Capitelli <victor.capitelli@scicrop.com>
2025-11-09 17:34:05 -0300

    feature: CORS;

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,13 @@
 import userRoutes from '#routes/userRoutes.ts'
 
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = 8080;
 
 app.listen(port, () => { console.log(`Backend server | Up & Running! ${port}`) })
 
+app.use(cors());
 app.use(express.json())
 app.use('/api', userRoutes)
+
